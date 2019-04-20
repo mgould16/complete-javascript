288e80f4241a334b0b79809e96cd7968eccf8a0f
Mark Gould <m.gould16@gmail.com>
2019-04-20 09:55:43 +0100

    completion of if/else statement

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -15,7 +15,13 @@ john_weight = 77;
 var mark_bmi = mark_weight / (mark_height^2)
 var john_bmi = john_weight / (john_height^2)
 
-var mark_greater = mark_bmi > john_bmi
+// var mark_greater = mark_bmi > john_bmi
 
-console.log(mark_greater)
+// console.log(mark_greater)
 
+
+if (mark_bmi > john_bmi) {
+  console.log("Mark has a greater BMI than John");
+} else {
+  console.log("John has a greater BMi than Mark")
+};
