a0f327d4aaa260803f7bb4ad7ebdf9c27a5add64
Mark Gould <m.gould16@gmail.com>
2019-04-20 09:46:28 +0100

    completion of exercise

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,21 @@
 console.log("Hello World!!")
+
+var mark_height, john_height;
+mark_height = 1.72;
+john_height = 1.66;
+
+var mark_weight, john_weight;
+mark_weight = 76;
+john_weight = 77;
+
+
+//  bmi = mass / height^2
+// bmi = mass / (height * height)
+
+var mark_bmi = mark_weight / (mark_height^2)
+var john_bmi = john_weight / (john_height^2)
+
+var mark_greater = mark_bmi > john_bmi
+
+console.log(mark_greater)
+
