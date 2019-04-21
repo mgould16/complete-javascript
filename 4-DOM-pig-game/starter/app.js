80ae7b877addc424c1347140fff66e9f892d136f
Mark Gould <m.gould16@gmail.com>
2019-04-21 13:38:45 +0100

    practice removing display of component on page

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -16,4 +16,13 @@ activePlayer = 0
 roundScore = 0
 dice = Math.floor((Math.random() * 6) + 1)
 
+// Hide a display
+document.querySelector('.dice').style.display = 'none';
+// roll dice and put score to active player
+document.querySelector("#current-" + activePlayer).textContent = dice;
+
+
+// insert html
+// document.querySelector("#current-" + activePlayer).innerHTML = '<em>' dice '</em>';
+
 
