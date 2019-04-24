259426e81978d87eb1696a77dd9f811599fa77f9
Mark Gould <m.gould16@gmail.com>
2019-04-24 09:17:02 +0100

    completion of hold button

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -15,6 +15,15 @@ scores = [0,0]
 activePlayer = 0
 roundScore = 0
 
+function nextPlayer () {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  document.querySelector('.dice').style.display = 'none';
+};
 
 // Hide a display
 document.querySelector('.dice').style.display = 'none';
@@ -32,14 +41,28 @@ document.querySelector(".btn-roll").addEventListener('click', function() {
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
+    nextPlayer();
   };
 })
+
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+  // Add score to global score
+  scores[activePlayer] += roundScore;
+  // Update the UI
+  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+  // Check if player won the game
+  if (scores[activePlayer] >= 10){
+    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  } else {
+    nextPlayer();
+  };
+});
+
+
 
 
 // insert html
