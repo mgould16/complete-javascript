156d3b83199296538744fd54412cdd1d16ea26de
Mark Gould <m.gould16@gmail.com>
2019-05-30 11:58:45 +0100

    callback functions explained

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -30,7 +30,6 @@ console.log(mark.calculateAge);
           // 2019 - this.yearOfBirth;
           // };
 
-
 var currentTime = new Date();
 var nowYear = currentTime.getFullYear();
 
@@ -46,3 +45,73 @@ var Film = function (title, year, director, rating) {
 
 var batman = new Film ("Batman", 1995, 'Wes Anderson', 4);
 console.log(batman.year);
+
+
+//new lecture on functions. we continue to use the arrayCalc function
+// throughout the next few functions, to make use of iterating over an array
+// and returning a new array, based off a "call back function"
+
+var years = [1990, 1985, 1987, 1996, 2003, 1982];
+
+// fn is a callback function. A callback is a function that is to be executed
+// after another function has finished executing — hence the name ‘call back’
+
+function arrayCalc(arr, fn) {
+  var results = [];
+  for (i = 0; i< arr.length; i++) {
+    results.push(fn(arr[i]));
+  };
+  return results;
+}
+
+function calcAge(el) {
+  return 2019 - el;
+}
+
+function isFullAge(el) {
+  return el >= 18;
+}
+
+function maxHeartRate(el) {
+  if (el >= 18) {
+    return Math.round(206.9 - (0.67* el));
+  }
+  else {
+    return -1;
+  }
+}
+
+var ages = arrayCalc(years, calcAge);
+console.log(ages);
+
+var fullAges = arrayCalc(ages, isFullAge);
+console.log(fullAges);
+
+var rates = arrayCalc(ages, maxHeartRate);
+console.log(rates);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
