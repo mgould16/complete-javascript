593a82ca272d1ecdc6032d1637f8931f507ce8ef
Mark Gould <m.gould16@gmail.com>
2019-05-30 12:27:08 +0100

    added anonymous functions, w/ explanation from course part 66

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -47,7 +47,8 @@ var batman = new Film ("Batman", 1995, 'Wes Anderson', 4);
 console.log(batman.year);
 
 
-//new lecture on functions. we continue to use the arrayCalc function
+// 65 Passing Functions as arguments
+// new lecture on functions. we continue to use the arrayCalc function
 // throughout the next few functions, to make use of iterating over an array
 // and returning a new array, based off a "call back function"
 
@@ -92,13 +93,37 @@ console.log(rates);
 
 
 
+// 66 Functions returning Functions (w/ anonymous functions)
+// There are 2 ways to call an anonymous function. An anonymous function is a
+// function within a function that is not named, let's have a look
 
 
+function interviewQuestion(job) {
+  if (job === "teacher") {
+    return function(name){
+      console.log(name + ", what do you teach?");
+    }
+  } else if (job === "designer") {
+    return function(name) {
+      console.log("so, why don't you explain to me a lil about UX, " + name);
+    }
+  } else {
+    console.log("What is your job?");
+  }
+}
 
 
+// so the first way to return is to store in a variable
+var teacherQuestion = interviewQuestion('teacher');
+// and we call it as follows
+teacherQuestion('John');
+
+var designerQuestion = interviewQuestion('designer');
+designerQuestion("Mark");
 
 
-
+// the second way to call the anonymous function is as follows:
+interviewQuestion('teacher')('jane');
 
 
 
